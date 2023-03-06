c9bf5d685172d06d0b84134f30a8a4cc8adc1b68
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-03-06 15:22:49 +0100

    replaced input button with semantic button tag

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,21 +1,37 @@
+import { useState } from "react";
 import "./main.css";
 
 const Main = () => {
+  const [inputValue, setInputValue] = useState();
+
+  const getInput = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleInput = (e) => {
+    e.preventDefault();
+    console.log(inputValue);
+  };
   return (
     <main className="main_container">
-      <form className="flex add_todo_cont">
+      <form className="flex primary_cont" onSubmit={handleInput}>
         <input
           type="text"
           name="todo_item"
           id="todo_item"
+          value={inputValue}
+          onChange={getInput}
           placeholder="Your todo goes here"
           className="todo_input"
         />
-        <input type="button" value="Add Todo" className="btn" />
+        <button type="submit" className="btn">
+          Add Todo
+        </button>
       </form>
-      <div className="flex add_todo_cont todo_items">
-        <div className="add_todo_cont todo_items">
-          <div> Complete my task </div>
+
+      <div className="flex margin-top primary_cont">
+        <div className="primary_cont todo_items">
+          <div> {inputValue} </div>
           <div>
             <i className="fa-solid fa-check pointer completed"></i>
           </div>
